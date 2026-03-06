05d18f7a7b2a15bc100aed9c8cf7f0fd5ae7250d
Daniel <dijalmaduartefleitas0@gmail.com>
2026-03-06 12:37:26 -0400

    Adicionando secao de inspecionar prdotuo

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const btnMenuAmburguer = document.querySelector("#menu-amburguer");
 const menuLateral = document.querySelector(".menu-lateral");
+const menuVertical = document.querySelector(".menu-vertical");
 const btnFecharMenu = document.querySelector(".btn-close");
+const botoesInspecionar = document.querySelectorAll(".botao-inspecionar");
+const botaoVoltar = document.querySelector(".botao-voltar");
+
 
 
 btnMenuAmburguer.addEventListener("click",function(){
@@ -14,7 +18,24 @@ btnFecharMenu.addEventListener("click",function(){
     menuLateral.classList.remove("menu-ativo");
 })
 
-// Inicializa o carrossel de produtos com Slick
+botoesInspecionar.forEach(botao =>{
+    botao.addEventListener("click",function(){
+        menuVertical.style.display = "block";
+
+        setTimeout(function(){
+        menuVertical.classList.add("menu-vertical-ativo");
+        },100)
+
+    })
+})
+botaoVoltar.addEventListener("click",function(){
+    menuVertical.classList.remove("menu-vertical-ativo");
+    setTimeout(function(){
+        menuVertical.style.display = "none";
+    },100)
+})
+
+
 $(document).ready(function(){
     $('#carouselprodutos').slick({
         slidesToShow: 2.5,
