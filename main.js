ee5b6fff9811eb210167121a4be62b3d1927315a
Daniel <dijalmaduartefleitas0@gmail.com>
2026-03-06 23:16:56 -0400

    Adicionando coleta de dados para inspecionar produto

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btnMenuAmburguer = document.querySelector("#menu-amburguer");
 const menuLateral = document.querySelector(".menu-lateral");
 const menuVertical = document.querySelector(".menu-vertical");
+const corpoMenuVertical = document.querySelector(".menu-vertical-corpo");
 const btnFecharMenu = document.querySelector(".btn-close");
 const botoesInspecionar = document.querySelectorAll(".botao-inspecionar");
 const botaoVoltar = document.querySelector(".botao-voltar");
@@ -8,7 +9,8 @@ const botaoVoltar = document.querySelector(".botao-voltar");
 
 
 btnMenuAmburguer.addEventListener("click",function(){
-    menuLateral.style.display = "block"
+    menuLateral.style.display = "block";
+    document.body.style.overflow = 'hidden';
     setTimeout(function(){
         menuLateral.classList.add("menu-ativo");
     },100)
@@ -16,6 +18,7 @@ btnMenuAmburguer.addEventListener("click",function(){
 })
 btnFecharMenu.addEventListener("click",function(){
     menuLateral.classList.remove("menu-ativo");
+    document.body.style.overflow = 'auto'; 
 })
 
 botoesInspecionar.forEach(botao =>{
@@ -27,18 +30,37 @@ botoesInspecionar.forEach(botao =>{
             nomeProduto : colunaProduto.querySelector("h6").innerText,
             valor : colunaProduto.querySelector("strong").innerText,
         }
-        menuVertical
-        
+        corpoMenuVertical.innerHTML = `<div class="container">
+                        <h3 class="mb-3 mt-3">${dados.nomeProduto}</h3>
+                        <h4 class="text-center mb-3">${dados.valor}</h4>
+                        <button class="btn btn-dark botao-adicionar-carrinho">+ Adicionar ao carrinho</button>
+                    <div class="container-favoritos-e-detalhes d-flex d-flex justify-content-between">
+                        <div class="botao-adicionar-favoritos d-flex justify-content-center align-items-center gap-2">
+                            <i class="fa-regular fa-heart"></i>
+                            <p class="mb-0">Adicionar ao favoritos</p>
+                        </div>
+                        <div class="botao-adicionar-favoritos d-flex justify-content-center align-items-center gap-2">
+                            <i class="fas fa-info-circle"></i>
+                            <p class="mb-0">Detalhes</p>
+                        </div>
+                    </div>`
         menuVertical.style.display = "block";
+        document.body.style.overflow = 'hidden'; // impede o scroll do body
+        $('.slider-for').slick('setPosition');
+        $('.slider-nav').slick('setPosition');
+
         setTimeout(function(){
         menuVertical.classList.add("menu-vertical-ativo");
+        document.querySelector(".faixa-alerta").style.top = "80px"
         },100)
     })
 })
 botaoVoltar.addEventListener("click",function(){
     menuVertical.classList.remove("menu-vertical-ativo");
+    document.body.style.overflow = 'auto'; // Restaura o scroll do body
     setTimeout(function(){
         menuVertical.style.display = "none";
+        document.querySelector(".faixa-alerta").style.top = "60px"
     },100)
 })
 
@@ -49,7 +71,7 @@ $(document).ready(function(){
         slidesToScroll: 1,
         infinite: false, 
         arrows: false,
-        dots: false,
+        dots: true,
     });
     $('.popup-link').magnificPopup({
         type: 'image',
